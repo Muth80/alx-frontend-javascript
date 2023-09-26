8d9f96561a16a3c93662b8811f6eeb9b0ddd4d1b
David Muindi <muindidiego@gmail.com>
2023-09-26 04:42:39 -0700

    Tasks

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,12 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Your api call or any code here;
+    // If success
+    resolve();
+
+    // If error
+    reject();
+  });  
+}
+
+export default getResponseFromAPI;
